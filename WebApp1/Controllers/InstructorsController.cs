521a08d22989ce7b575ab5464b8fab3063bfe1d9
Ali elnoby <91030388+Ali011A@users.noreply.github.com>
2025-04-10 00:45:37 +0200

    Day4

--- WebApp1/Controllers/InstructorsController.cs
+++ WebApp1/Controllers/InstructorsController.cs
@@ -51,12 +51,121 @@ namespace WebApp1.Controllers
                                   Salary = INS.salary,
                                   Address = INS.Address,
                                   DepName = DEP.Name,
-                                  CoursName = course.Name
+                                  CoursName = course.Name,
+                                  CourseId= INS.CourseId,
+                                  DepartmentId= INS.DepartmentId
+
                               }
                             ).FirstOrDefault();
 
             return View("ShowDetails", instructor);
            
         }
+
+        #region AddInstructor
+        //endpoint url : /Instructors/Create
+        public IActionResult Create()
+        {
+            ViewBag.Departments = trainingAcademy.Departments.ToList();
+            ViewBag.Courses = trainingAcademy.Courses.ToList();
+            return View("Create");
+        }
+
+        //endpoint url : /Instructors/SaveInstructor
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SaveInstructor(InstructorViewModel ins)
+        {
+        
+            // Check if the instructor has valid data
+            if (ins.Name != null && ins.Image != null && ins.Salary > 7000 && ins.Address 
+                != null && ins.CourseId != 0 && ins.DepartmentId != 0)
+            {
+               
+                trainingAcademy.Instructors.Add(new Instructor
+                {
+                    Name = ins.Name,
+                    Image = ins.Image,
+                    salary = ins.Salary,
+                    Address = ins.Address,
+                    CourseId  = ins.CourseId, 
+                    DepartmentId =ins.DepartmentId
+                    
+                });
+                trainingAcademy.SaveChanges();
+                return RedirectToAction("ShowAll");
+
+            }
+           
+           
+                ViewBag.Departments = trainingAcademy.Departments.ToList();
+                ViewBag.Courses = trainingAcademy.Courses.ToList();
+                return View("Create",ins);
+            
+          
+        }
+        #endregion
+
+        #region EditInstructor
+        //endpoint url : /Instructors/Edit/{id}
+
+        public IActionResult Edit(int id)
+        {
+            var instructor = (from INS in trainingAcademy.Instructors
+                              join DEP in trainingAcademy.Departments
+                              on INS.DepartmentId equals DEP.id
+                              join course in trainingAcademy.Courses
+                              on INS.CourseId equals course.Id
+                              where INS.id == id
+                              select new InstructorViewModel
+                              {
+                                  Id = INS.id,
+                                  Name = INS.Name,
+                                  Image = INS.Image,
+                                  Salary = INS.salary,
+                                  Address = INS.Address,
+                                  DepName = DEP.Name,
+                                  CoursName = course.Name,
+                                  CourseId = INS.CourseId,
+                                  DepartmentId = INS.DepartmentId
+                              }
+                            ).FirstOrDefault();
+              ViewBag.Departments = trainingAcademy.Departments.ToList();
+
+            ViewBag.Courses = trainingAcademy.Courses.ToList();
+            return View("Edit", instructor);
+        }
+        [HttpPost]
+       
+        //endpoint url : /Instructors/EditInstructor
+        public IActionResult EditInstructor(InstructorViewModel iTns)
+        {
+
+            // Check if the instructor has valid data
+            if (iTns.Name != null && iTns.Image != null && iTns.Salary > 7000 && iTns.Address
+                != null && iTns.CourseId != 0 && iTns.DepartmentId != 0)
+            {
+               
+                var instructor = trainingAcademy.Instructors.FirstOrDefault(i => i.id == iTns.Id);
+                //if (instructor != null)
+                //{ }
+                    instructor.Name = iTns.Name;
+                    instructor.Image = iTns.Image;
+                    instructor.salary = iTns.Salary;
+                    instructor.Address = iTns.Address;
+                    instructor.CourseId = iTns.CourseId;
+                    instructor.DepartmentId = iTns.DepartmentId;
+                    trainingAcademy.SaveChanges();
+                
+                return RedirectToAction("ShowAll");
+            }
+            ViewBag.Departments = trainingAcademy.Departments.ToList();
+            ViewBag.Courses = trainingAcademy.Courses.ToList();
+            return View("Edit", iTns);
+
+
+        }
+
+        #endregion
     }
 }
--- WebApp1/Models/InstructorViewModel.cs
+++ WebApp1/Models/InstructorViewModel.cs
@@ -7,7 +7,9 @@
         public string Image { get; set; }
         public double Salary { get; set; }
         public string Address { get; set; }
+        public int CourseId { get; set; }
         public string CoursName { get; set; }
+        public int DepartmentId { get; set; }
         public string DepName { get; set; }
     }
 }
