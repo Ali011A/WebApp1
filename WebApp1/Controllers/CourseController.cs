abd91f1daaacc24ad0c14d70f78d9ef9f0ccdf30
Ali elnoby <91030388+Ali011A@users.noreply.github.com>
2025-04-12 23:30:26 +0200

    Day5

--- WebApp1/Controllers/CourseController.cs
+++ WebApp1/Controllers/CourseController.cs
@@ -7,7 +7,7 @@ namespace WebApp1.Controllers
     {
         Training_Academy Training_Academy = new Training_Academy();
         // Endpoint url : /Course/Result?cid=1
-        public IActionResult Result(int cid) 
+        public IActionResult Result(int id) 
         {
 
             var  course = from cr in Training_Academy.CrsResults
@@ -15,7 +15,7 @@ namespace WebApp1.Controllers
                           on cr.TraineeId equals tr.Id
                           join c in Training_Academy.Courses
                           on cr.CourseId equals c.Id
-                          where c.Id == cid
+                          where c.Id == id
                           select new CourseViewModel
                           {
                               TraineeName = tr.Name,
@@ -32,5 +32,122 @@ namespace WebApp1.Controllers
 
             return View( "Result", course.ToList());
         }
+        // Endpoint url : /Course/AllCourses
+        public IActionResult AllCourses()
+        {
+            var courses = from C in Training_Academy.Courses
+                          join d in Training_Academy.Departments
+                            on C.DepartmentId equals d.id
+                          select new CourseViewModel
+                          {
+                              Id = C.Id,
+                              CourseName = C.Name,
+                              hours = C.hours,
+                              minDegree = C.minDegree,
+                              Degree = C.degree,
+                              DepName = d.Name
+                             
+                          };
+
+            return View("AllCourses", courses.ToList());
+}
+
+        #region AddCourse
+        // Endpoint url : /Course/AddCourses
+        public IActionResult AddCourses()
+        {
+            CourseViewModel courses= new CourseViewModel();
+          courses.DepList = Training_Academy.Departments.ToList();
+            return View("AddCourses", courses);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SaveCourse( CourseViewModel coursess)
+        {
+            if(coursess.CourseName != null && coursess.DepartmentId != 0 &&
+                coursess.hours != 0 && coursess.minDegree != 0 && coursess.minDegree !=0
+                )
+            {
+                Training_Academy.Courses.Add(new Course
+                {
+                    Name = coursess.CourseName,
+                    hours= coursess.hours,
+                    minDegree=coursess.minDegree,
+                    DepartmentId = coursess.DepartmentId,
+                    degree = coursess.Degree
+               
+                 
+
+                });
+
+                Training_Academy.SaveChanges();
+                return RedirectToAction("AllCourses");
+            }
+
+
+            CourseViewModel courseViewModel = new CourseViewModel();
+            courseViewModel.DepList = Training_Academy.Departments.ToList();
+            return View("AddCourses", courseViewModel);
+
+        }
+        #endregion
+
+        #region Edit course
+        // Endpoint url : /Course/EditCourse/{id}
+        //[HttpPost]
+        public IActionResult EditCourse(int id)
+        {
+
+            var course = (from C in Training_Academy.Courses
+                          join d in Training_Academy.Departments
+                          on C.DepartmentId equals d.id
+                          where C.Id == id
+                          select new CourseViewModel
+                          {
+                              Id = C.Id,
+                              CourseName = C.Name,
+                              hours = C.hours,
+                              minDegree = C.minDegree,
+                              Degree = C.degree,
+                              DepName = d.Name,
+                              DepartmentId = C.DepartmentId
+
+                          }).FirstOrDefault();
+            course.DepList = Training_Academy.Departments.ToList();
+            return View("EditCourse", course);
+        }
+
+        public IActionResult SEditCourse(CourseViewModel course)
+        {
+
+
+            if (course.CourseName != null && course.DepartmentId != 0 &&
+                course.hours != 0 && course.minDegree != 0)
+            {
+                Training_Academy.Courses.Update(new Course
+                {
+                    Id = course.Id,
+                    Name = course.CourseName,
+                    hours = course.hours,
+                    minDegree = course.minDegree,
+                    DepartmentId = course.DepartmentId,
+                    degree = course.Degree
+                });
+                Training_Academy.SaveChanges();
+                return RedirectToAction("AllCourses");
+
+
+
+            }
+            CourseViewModel courseViewModel = new CourseViewModel();
+            courseViewModel.DepList = Training_Academy.Departments.ToList();
+            return View("EditCourse", courseViewModel);
+       
+
+
+        }
+        #endregion
     }
 }
--- WebApp1/Models/CourseViewModel.cs
+++ WebApp1/Models/CourseViewModel.cs
@@ -1,12 +1,26 @@
-﻿namespace WebApp1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp1.Models
 {
     public class CourseViewModel
     {
+        public int Id { get; set; }
         public string? TraineeName { get; set; }
+        [Display(Name = "Course Name")]
         public string? CourseName { get; set; } // CourseName
         public int Degree { get; set; } // Degree
         public string? Status { get; set; }
         public string? color { get; set; }
         public string? Image { get; set; } // Image
+        [Display(Name = "Department Name")]
+        public int DepartmentId { get; set; }
+        
+        public string? DepName { get; set; } // Department Name
+        public List<Department>? DepList { get; set; } // list of departments
+        [Display(Name = " minamum degree")]
+        public int minDegree { get; set; }
+        [Display(Name = "Course hours")]
+        public int hours { get; set; } // hours
+
     }
 }
